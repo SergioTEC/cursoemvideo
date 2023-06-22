d58e08c2a5191d670899e5f066aa8fd354c89d91
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2023-06-21 23:37:59 -0300

    Finalizando o exercício1

--- aula012ex/ex01/script.js
+++ aula012ex/ex01/script.js
@@ -4,14 +4,17 @@ function carregar(){
 
     var date = new Date()
     var hora = date.getHours()
-
+    
     mostraHoras.innerHTML = `<strong>Agora são ${hora} horas.</strong>`
 
     if(hora >= 0 & hora < 12){
         imagem.src = "/aula012ex/ex01/contents/manha01.jpg"
+        document.body.style.background = '#CCC870'
     } else if(hora >= 12 & hora < 18){
         imagem.src = "/aula012ex/ex01/contents/tarde01.jpg"
+        document.body.style.background = '#6A5C23'
     } else {
         imagem.src = "/aula012ex/ex01/contents/noite01.jpg"
+        document.body.style.background = '#1C233B'
     }
 }
